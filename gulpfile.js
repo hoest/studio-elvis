56457551426652dc8ba76e02aafafabaa7fc0a21
Jelle de Jong <jelle@hoest.nl>
2016-03-27 17:45:31 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sourcemaps = require('gulp-sourcemaps');
+var stylint = require('gulp-stylint');
+var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+
+var jeet = require('jeet');
+var rupture = require('rupture');
+var axis = require('axis');
+var autoprefixer = require('autoprefixer-stylus');
+
+gulp.task('clean', function () {
+  return gulp.src('./_public/**/*', {read: false})
+             .pipe(clean({
+                force: true
+              }));
+});
+
+gulp.task('stylus', function () {
+  return gulp.src(['./src/css/*.styl', '!./src/css/_*.styl'])
+             .pipe(stylint())
+             .pipe(stylus({
+               compress: true,
+               use: [rupture(), jeet(), axis(), autoprefixer()]
+             }))
+             .pipe(concat('style.css'))
+             .pipe(gulp.dest('./_public/'));
+});
+
+gulp.task('concat', function () {
+  return gulp.src(['./_public/css/*.css'])
+             .pipe(concat('styles.css'))
+             .pipe(gulp.dest('./_public/'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./src/js/*.js')
+             .pipe(jshint())
+             .pipe(jshint.reporter('jshint-stylish'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./_public/js/'));
+});
+
+gulp.task('html', function() {
+  return gulp.src(['./src/*.html', './src/*.php'])
+             .pipe(gulp.dest('./_public'));
+});
+
+gulp.task('images', function() {
+  return gulp.src(['./src/images/**/*'])
+             .pipe(imagemin({
+               progressive: true,
+               svgoPlugins: [{removeViewBox: false}],
+             }))
+             .pipe(gulp.dest('./_public/images/'));
+});
+
+gulp.task('default', ['stylus', 'js', 'html', 'images']);
