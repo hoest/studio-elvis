76f02df4ec7f20d62daba64b2c54bdc9a2b4f90d
Jelle de Jong <jelle@hoest.nl>
2016-04-09 16:08:08 +0200

    Linkjes toegevoegd

--- src/sidebar.php
+++ src/sidebar.php
@@ -2,17 +2,17 @@
   <?php wp_nav_menu(array('container' => '', 'theme_location' => 'primary')); ?>
   <ul class="social">
     <li>
-      <a href>
+      <a target="_blank" href="https://www.facebook.com/studioelvis/">
         <i class="fa fa-facebook"></i>
       </a>
     </li>
     <li>
-      <a href>
+      <a target="_blank" href="https://twitter.com/studio_elvis_nl">
         <i class="fa fa-twitter"></i>
       </a>
     </li>
     <li>
-      <a href>
+      <a target="_blank" href="https://www.linkedin.com/company/studio-elvis">
         <i class="fa fa-linkedin"></i>
       </a>
     </li>
