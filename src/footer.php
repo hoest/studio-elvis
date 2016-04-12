3141df6d63fb3250e616113c6adcb0f26a436cce
Jelle de Jong <jelle@hoest.nl>
2016-04-12 20:00:11 +0200

    Kleine aanpassingen

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,17 @@
       <footer>&#169; 2014-<?php echo date("Y"); ?> - Studio Elvis</footer>
     </div>
 
+    <script>
+      (function(d) {
+        var config = {
+          kitId: 'nlf5kcs',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);
+    </script>
+
     <script src="//code.jquery.com/jquery-2.2.2.min.js"
       integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI="
       crossorigin="anonymous"></script>
